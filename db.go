e97f9429c8eaa6f388a52b116e66a40ed4d0bd79
Aiden X <caichao.xu@gmail.com>
2022-02-24 22:44:58 +0800

    Add get dsn function

--- db.go
+++ db.go
@@ -11,26 +11,20 @@ import (
 var gormDB *gorm.DB
 
 func LoadGormDB() {
-	host := viper.GetString("mysql.host")
-	port := viper.GetInt("mysql.port")
-	username := viper.GetString("mysql.username")
-	password := viper.GetString("mysql.password")
-	database := viper.GetString("mysql.database")
-
-	dsn := GetDSN(username, password, host, port, database)
+	dsn := GetDSN()
 
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{
 		Logger: logger.Default.LogMode(logger.Silent),
 	})
 
 	if err != nil {
-		panic(fmt.Errorf("gorm open: %w", err))
+		panic(fmt.Errorf("gorm open(%s): %w", dsn, err))
 	}
 
 	gormDB = db
 }
 
-func GetDSN(username, password, host string, port int, database string) string {
+func getDSN(username, password, host string, port int, database string) string {
 	return fmt.Sprintf("%s:%s@tcp(%s:%d)/%s?charset=utf8mb4&parseTime=True&loc=Local",
 		username,
 		password,
@@ -38,4 +32,14 @@ func GetDSN(username, password, host string, port int, database string) string {
 		port,
 		database,
 	)
+}
+
+func GetDSN() string {
+	host := viper.GetString("mysql.host")
+	port := viper.GetInt("mysql.port")
+	username := viper.GetString("mysql.username")
+	password := viper.GetString("mysql.password")
+	database := viper.GetString("mysql.database")
+
+	return getDSN(username, password, host, port, database)
 }
